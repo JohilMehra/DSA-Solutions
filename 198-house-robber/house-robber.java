2fbb2fd1bd400f82caf9bfb9f77451efc39ac11a
Johil Mehra <johilmehra1@gmail.com>
2026-06-18 19:42:03 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (87.04%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,6 +1,6 @@
 class Solution {
     public int rob(int[] nums) {
-        int n=nums.length;
+    int n=nums.length;
         if(n==1) return nums[0];
         if(n==2) return Math.max(nums[0],nums[1]);
         int dp[]=new int[n];
