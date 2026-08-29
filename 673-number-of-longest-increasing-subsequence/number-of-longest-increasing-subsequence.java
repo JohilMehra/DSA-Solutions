0baea072e68c37d496f3ba7fde75d767a76719db
Johil Mehra <johilmehra1@gmail.com>
2026-08-29 17:06:40 +0530

    Time: 22 ms (40.05%) | Memory: 45.7 MB (25.36%) - LeetSync

--- 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.java
+++ 673-number-of-longest-increasing-subsequence/number-of-longest-increasing-subsequence.java
@@ -1,32 +1,36 @@
 class Solution {
     public int findNumberOfLIS(int[] nums) {
-        int n=nums.length;
-        int[] length=new int[n];
-        int[] count=new int[n];
-        Arrays.fill(length,1);
+        int n = nums.length;
+        int len[] = new int[n]; //dp
+        int count[] = new int[n];
+        Arrays.fill(len,1);
         Arrays.fill(count,1);
-
-        int maxlen=1;
-        for(int i=0;i<n;i++){
+        
+        int max=1;
+        for(int i=1;i<n;i++){
             for(int j=0;j<i;j++){
-                if(nums[j]<nums[i]){
-                    if(length[j]+1 > length[i]){
-                        length[i]=length[j]+1;
-                        count[i]=count[j];
-                    }else if(length[j]+1 == length[i]){
-                        count[i]+=count[j];
-                    }
+                if(nums[j]<nums[i] && len[j]+1 > len[i]){
+                    //if it increases the existing length
+                    //update the len
+                    len[i]=len[j]+1;
+                    //assign update count
+                    count[i]=count[j];
+                }else if(nums[j] < nums[i] && len[j]+1 == len[i]){
+                    //if same length LIS repeats again
+                    //increase the count
+                    count[i] += count[j];
                 }
             }
-            maxlen=Math.max(maxlen,length[i]);
+            max=Math.max(len[i],max);
         }
-        int result=0;
+        //calculate total LIS :-  it can be at different location
+        //like in ex:2 at LIS:1 and we have to count all counts with having len=1 
+        int total=0;
         for(int i=0;i<n;i++){
-            if(length[i] == maxlen){
-                result+=count[i];
+            if(len[i]==max){
+                total+=count[i];
             }
         }
-
-        return result;
+        return total;
     }
 }
